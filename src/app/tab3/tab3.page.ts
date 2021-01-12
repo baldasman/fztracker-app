ea2da5a3c706eed0cdd6b1686457dc338bcc202b
tfuzo <tfuzo@hotmail.com>
2021-01-12 00:48:40 +0000

    com pass

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,55 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  todo = {
+    local: '',
+    movelName: '',
+    mail: '',
+    pass: '',
+  };
+ 
   constructor() {}
 
+  ngOnInit() {
+    document.getElementById('123').hidden = true;
+    document.getElementById('122').hidden = false;
+  };
+
+
+login(form){
+ 
+  
+ 
+
+  if ( (Object.entries(form.value).toString()) == "email,1,password,2") {
+         console.log("correcto", form)
+         document.getElementById('123').hidden = false;
+         document.getElementById('122').hidden = true;
+          setTimeout(function(){
+            console.log("agora")
+            document.getElementById('123').hidden = true;
+            document.getElementById('122').hidden = false;
+            form.reset();
+          
+          },20000);
+  };
+  
+
+
+  
 }
+
+  saveMovel(){console.log ("teste salvar",this.todo)
+
+   // utilizar estes dados para cadastrar equipamento  e guardar local de registo. 
+
+
+
+  }
+
+
+
+
+}
+
+
