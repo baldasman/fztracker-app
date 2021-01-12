d39c6124eb6b1c4186312543833f0bb68990c7c7
tfuzo <tfuzo@hotmail.com>
2021-01-12 12:09:31 +0000

    build

--- platforms/android/app/src/main/assets/www/tab3-tab3-module-es2015.js
+++ platforms/android/app/src/main/assets/www/tab3-tab3-module-es2015.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<ion-header>\r\n  <ion-toolbar>\r\n    <ion-title>\r\n      Tab Three\r\n    </ion-title>\r\n  </ion-toolbar>\r\n</ion-header>\r\n\r\n<ion-content></ion-content>\r\n"
+module.exports = "<ion-content  class=\"ion-padding\">\r\n  <form  id=\"122\" #form=\"ngForm\" (ngSubmit)=\"login(form)\">\r\n    <ion-grid>\r\n      <ion-row color=\"primary\" class=\"ion-justify-content-center\">\r\n        <ion-col class=\"ion-align-self-center\" size-md=\"6\" size-lg=\"5\" size-xs=\"12\" >\r\n          <div class=\"ion-text-center\">\r\n            <h3>Login</h3>\r\n          </div>\r\n          <div class=\"ion-padding\">\r\n            <ion-item>\r\n              <ion-input name=\"email\" type=\"email\" placeholder=\"email\" ngModel required></ion-input>\r\n            </ion-item>\r\n            <ion-item>\r\n              <ion-input name=\"password\" type=\"password\" placeholder=\"Password\" ngModel required></ion-input>\r\n            </ion-item>\r\n          </div>\r\n          <div class=\"ion-padding\">\r\n            <ion-button size=\"large\" type=\"submit\" (click)=\"login(form)\" expand=\"block\">Login</ion-button>\r\n          </div>\r\n        </ion-col>\r\n      </ion-row>\r\n    </ion-grid>\r\n  </form>\r\n\r\n\r\n\r\n<form (ngSubmit)=\"saveMovel()\" id=\"123\">\r\n\r\n    <ion-grid>\r\n      <ion-row color=\"primary\" class=\"ion-justify-content-center\">\r\n        <ion-col class=\"ion-align-self-center\" size-md=\"6\" size-lg=\"5\" size-xs=\"12\">\r\n            \r\n        <ion-item>\r\n              <ion-label>Local</ion-label>\r\n              <ion-select name=\"local\" value=\"CF-Alfeite\" okText=\"ok\" cancelText=\"sair\"  [(ngModel)]=\"todo.local\">\r\n                  <ion-select-option value=\"CF-Alfeite\">CF-Alfeite</ion-select-option>\r\n                  <ion-select-option value=\"CF-Escola\">CF-Escola</ion-select-option>\r\n             </ion-select>\r\n\r\n           </ion-item>\r\n            <ion-item>\r\n            <ion-label>Nome do dispoditivo</ion-label>\r\n            <ion-input name=\"movel\" type=\"text\" placeholder=\"ex. Movel1\"   [(ngModel)]=\"todo.movelName\" required></ion-input>\r\n           </ion-item>\r\n            <ion-item style=\"padding-top:20px\">\r\n            <ion-input name=\"mail\" type=\"email\" placeholder=\"email\"  [(ngModel)]=\"todo.mail\" required></ion-input>\r\n            </ion-item>\r\n\r\n            <ion-item>\r\n            <ion-input name=\"pass\" type=\"password\" placeholder=\"Password\"   [(ngModel)]=\"todo.pass\" required></ion-input>\r\n            </ion-item>\r\n            <div class=\"ion-padding\">\r\n            <ion-button size=\"large\" type=\"submit\" (click)=\"saveMovel()\" expand=\"block\">Login</ion-button>\r\n            </div>\r\n\r\n      </ion-col>\r\n     </ion-row>\r\n      \r\n  </ion-grid>\r\n\r\n\r\n\r\n\r\n</form>\r\n\r\n</ion-content>\r\n"
 
 /***/ }),
 
@@ -79,7 +79,37 @@ __webpack_require__.r(__webpack_exports__);
 
 
 let Tab3Page = class Tab3Page {
-    constructor() { }
+    constructor() {
+        this.todo = {
+            local: '',
+            movelName: '',
+            mail: '',
+            pass: '',
+        };
+    }
+    ngOnInit() {
+        document.getElementById('123').hidden = true;
+        document.getElementById('122').hidden = false;
+    }
+    ;
+    login(form) {
+        if ((Object.entries(form.value).toString()) == "email,1,password,2") {
+            console.log("correcto", form);
+            document.getElementById('123').hidden = false;
+            document.getElementById('122').hidden = true;
+            setTimeout(function () {
+                console.log("agora");
+                document.getElementById('123').hidden = true;
+                document.getElementById('122').hidden = false;
+                form.reset();
+            }, 20000);
+        }
+        ;
+    }
+    saveMovel() {
+        console.log("teste salvar", this.todo);
+        // utilizar estes dados para cadastrar equipamento  e guardar local de registo. 
+    }
 };
 Tab3Page = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
     Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -68,9 +68,10 @@ export class Tab1Page {
       
 
       // Search card by id
-      // this.cardId = this.nfc.bytesToHexString(event.tag.id);
+      this.cardId = this.nfc.bytesToHexString(event.tag.id);
       this.cardNumber = null;
-      this.cardId = '0412FD1AE66C81';
+    //  this.cardId = '0412FD1AE66C81';
+    console.log('nfc', this.cardId);
       this.searchCard();
     });
   }
