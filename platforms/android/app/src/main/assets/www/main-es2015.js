72b158bbc5c2d3678c50d92cea062345640e9a31
tfuzo <tfuzo@hotmail.com>
2021-03-04 14:21:43 +0000

    last

--- platforms/android/app/src/main/assets/www/main-es2015.js
+++ platforms/android/app/src/main/assets/www/main-es2015.js
@@ -619,22 +619,12 @@ AppModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// alterar o token e o IP da Maquina.
 const environment = {
     production: false,
-    api: 'http://192.168.1.103:8000',
+    api: 'http://192.168.1.184:8000',
     token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoSWQiOiJtZUBmenRyYWNrZXIubG9jYWwiLCJzZXNzaW9uSWQiOiI5M2UzZDA1MC02NDg5LTExZWItODljYy1iMTk3MDAwNTIxNGIiLCJzZXNzaW9uVHlwZSI6InBvcnRhbCIsIm51bWJlck9mTG9naW5zIjoyOCwibGFzdExvZ2luRGF0ZSI6MTYxMjE4Mjc2MiwidHRsIjoxNzI4MDAsImNyZWF0ZWRBdCI6MTYxMjE4Mjc2MiwiaWF0IjoxNjEyMTgyNzYxfQ.NXXXj7mD3cFxXqylaB-9NRpnFm5ERtPXAQqnMd1yzbkNOaexQ7NvsU3TeUshnoq_8kQHi9G6aEEQscUncXBo3LTJ0DK7JoZEVLoqpo8QkKCQMUMm1HLxwnYMNMPZPpHMbZZidKUvb07RZ1k7pAgspKYlzJrmITaFyVSxcipQN2dpgNdy7vbbMwKaresyxF_2NYFG8oz5X2PsAC17VlPLmJW9ZOdwk21NRROB0yyUA6u-7ysqWcrG_ZQF8tsbCZlM8xjDP2UnSCtUQsep-_ZbxjXwNpyNS24sZKEILCrh9g4nrJnAMxAhw-aKaWA2LJBxPnxi95i_fOuboH1aShzJbS7rny96fb1g9ae8yFDPQJujB3rIEg9AmuNbsftxWdLAhx2O7EsPPcW7pFAnPAMaIwRPJBalUEL3wzCz8Iuietu11XjHMB9dePmpuq4neDYypz6Dj-cCCCZSfvPH38aGIvJmruo70JGmSjuG_Io-k5zHeYzN_GJtK-srIsgcAReLMY6HIiLUjyTlHR27_ULCGvyHWT-qlMBzQi8qg0pXA2yqAIo5AQ0vj-fkeuJJwNJLfBg77AwjH6qrpFon2ZSaEStRmrlr_rW4Nl7KcuS-usfRdx4Ke6RFluQXSwRLUKprAwm5mak8eUIVy2NQNsRbI6a5tE0AuCuAd7iCGtchbN0'
 };
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
 
 
 /***/ }),
