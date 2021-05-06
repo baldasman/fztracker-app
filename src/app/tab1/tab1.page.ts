d0278275ae3261614763aab452cefb3f5ad5c3ea
tomé sousa <tfuzo@hotmail.com>
2021-05-06 08:24:02 +0100

    Update tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -143,9 +143,9 @@ export class Tab1Page {
         this.changeRef.detectChanges();
         return;
       }
-
+      console.log();
       const entity = rawData.data.entities[0];
-      console.log('a entidade' +entity)
+      console.log('a entidade' +entity);
       this.cardInfo = entity.cardNumber;
       this.color = '';
       this.photo = `${this.apiService.getApi()}/assets/userPhotos/${entity.serial}.bmp`;
